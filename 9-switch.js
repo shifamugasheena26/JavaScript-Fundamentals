e72b0262c24ff03f9b452a9c402dfab388620ff2
Shifa Mugasheena.A <shifamugasheena26@gmail.com>
2026-07-25 19:39:57 +0530

    Feat:added a switch statement

---
+++ 9-switch.js
@@ -0,0 +1,13 @@
+const color = "green";
+
+switch(color){
+    case "green":
+        console.log(`Im green`);
+        break;
+    case "blue" :
+        console.log(`Im blue`);
+        break;
+    default:
+        console.log(`I don't know the color`)    ;
+}
+//Im green
