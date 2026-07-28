8972fe06e3b6608a82043e37649960aa06881098
Shifa Mugasheena.A <shifamugasheena26@gmail.com>
2026-07-28 18:46:04 +0530

    Feat:added conversion

--- typeconversion_basic.js
+++ typeconversion_basic.js
@@ -9,6 +9,7 @@ let c = String(10+3);
 console.log(c);
 console.log(typeof c); //String 13
 
+
 let d = Number("5" + "5");
 console.log(d); 
 console.log(typeof d); //number 55
@@ -31,3 +32,27 @@ console.log(Boolean(0));   // false
 console.log(Boolean(""));  // false
 console.log(Boolean("hi"));//true
 console.log(Boolean()); //false
+
+
+//implicit conversion 
+
+console.log("5" + 1);   // "51" - string + number -> string aagum
+console.log("5" - 1);   // 4    - string - number -> number aagum
+console.log("5" * "2"); // 10   - rendும் string, aana * ku number aagum
+console.log(1 + true);  // 2    - true -> 1 aagum
+console.log(1 + false); // 1    - false -> 0 aagum
+
+
+//list all falsy value and log their boolean conversion
+let falsyvalues = [0 , "" , false , null , NaN ,undefined];
+for(let val of falsyvalues){
+    console.log(val , "->" , Boolean(val));
+}
+
+//output
+// 0 '->' false
+// ""-> false
+// false '->' false
+// null '->' false
+//  NaN '->' false
+// undefined '->' false
