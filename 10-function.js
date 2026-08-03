f6af311e7ed6315a11dede63c2ad07c5290018dc
Shifa Mugasheena.A <shifamugasheena26@gmail.com>
2026-08-03 17:54:32 +0530

    Feat:added array

--- 10-function.js
+++ 10-function.js
@@ -12,3 +12,27 @@ function add(a,b){
 }
 let result = add(20,30);
 console.log(result);
+
+//closure funtion inner and outer function 
+
+function outer(){
+    var b = 10;
+
+    function inner(){
+        var a = 25;
+        console.log(a+b);
+    }
+    return inner;
+}
+let innerfunction = outer();
+innerfunction(); //35
+
+
+//arrays
+
+let color = ["red" , "orange" , "white"];
+console.log(color);
+
+
+let color1 = new Array('red' , 'orange',"white");
+console.log(color1);
