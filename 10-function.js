f71769560ef566ae933a44ba8244e773eb7cb2df
Shifa Mugasheena.A <shifamugasheena26@gmail.com>
2026-08-03 17:24:07 +0530

    Feat: added function

---
+++ 10-function.js
@@ -0,0 +1,14 @@
+//function with parameter
+
+function greet(name){
+    console.log(`hello ${name}`);
+}
+greet("shifa Mugasheena");
+
+//function with return value
+
+function add(a,b){
+    return a+b;
+}
+let result = add(20,30);
+console.log(result);
---
+++ objecttask.js
@@ -0,0 +1,10 @@
+//1.create  a book object with title , author , and  price properties
+
+let book = {
+    title:"Harry Potter",
+    author:"J.K. Rowling",
+    price:500
+};
+console.log(book);
+
+//2. add a method to an object to return its formatted description
