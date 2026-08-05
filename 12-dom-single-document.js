8d8acd024ac649e231343a5fee71f902574294cf
Shifa Mugasheena.A <shifamugasheena26@gmail.com>
2026-08-05 14:56:58 +0530

    Feat:added single element

---
+++ 12-dom-single-document.js
@@ -0,0 +1,32 @@
+//document.getElementById();
+
+console.log(document.getElementById("head"));
+
+const test = document.getElementById("head");
+console.log(test);
+
+//style changes
+
+console.log(test.style);
+
+test.style.color="red";
+
+//change content
+console.log(test.innertext);
+
+test.innertest="hello shifa";
+
+//add tag 
+
+console.log(test.innerHTML);
+
+test.innerHtML="<h1>hello</h1>";
+
+
+
+//document.querySelector()
+
+console.log(document.querySelector("#head"));
+
+console.log(document.querySelector(".head"));
+
